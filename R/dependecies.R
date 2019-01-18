6384339ec12325f34b6707388bc4a5d9b3b00401
Javier Luraschi <javierluraschi@hotmail.com>
2019-01-17 23:19:01 -0600

    use jars not spark packages

--- R/dependecies.R
+++ R/dependecies.R
@@ -1,8 +1,6 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
   sparklyr::spark_dependency(
-    # geomesa package: https://mvnrepository.com/artifact/org.locationtech.geomesa/geomesa-fs-dist_2.11/2.1.1
-    # grouId:artifact:version
-    packages = "org.locationtech.geomesa:geomesa-fs-dist_2.11:2.1.1"
+    jars = system.file("jars/jai_core-1.1.3.jar", package = "sparksf")
   )
 }
 
